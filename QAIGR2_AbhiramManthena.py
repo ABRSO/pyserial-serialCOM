1990fcd91428f6187928bad505895834457ec90b
Abhiram Manthena <67962053+ABRSO@users.noreply.github.com>
2024-07-12 18:52:45 +0530

    Update QAIGR2_AbhiramManthena.py

--- QAIGR2_AbhiramManthena.py
+++ QAIGR2_AbhiramManthena.py
@@ -2,17 +2,17 @@ import serial
 import time
 
 # Serial port configuration
-ser = serial.Serial('COM3', 115200, timeout=1)  # COM3 is my system's serial port, please change it as required
+ser = serial.Serial('COM3', 115200, timeout=1)  # COM3 is my system's serial port
 
 def send_and_receive_message(message):
     ser.write((message + '\n').encode())  # Sends the message and encodes the string into bits
     time.sleep(2)  # Wait for the response
-    response = ser.readline().decode().strip()  # Reads the response and decodes the bits back into string
+    response = ser.readline().decode().strip()
     return response
 
 if __name__ == "__main__":
     message = input("Enter the message to send: ")
-    response = send_and_receive_message(message) # Calls the function with input message
+    response = send_and_receive_message(message)
     print(f"Sent: {message}")
     print(f"Received: {response} \nSerial port closed")
     ser.close() # Closes the serial port
